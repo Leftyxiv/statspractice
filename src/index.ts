3b823d4000850c444481913406efb60621380b7b
Leftyxiv <manny@aledoux.net>
2021-08-19 12:12:47 -0400

    added a generic type to the abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 import { matchResult } from './matchResult';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
 
 
