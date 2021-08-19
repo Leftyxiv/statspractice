ee6bf7a6fa50582bc8ab0b7dcf14bc6e4081c660
Leftyxiv <manny@aledoux.net>
2021-08-19 09:29:29 -0400

    added function to parse dates

---
+++ build/index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+var matchResult;
+(function (matchResult) {
+    matchResult["HomeWin"] = "H";
+    matchResult["AwayWin"] = "A";
+    matchResult["Draw"] = "D";
+})(matchResult || (matchResult = {}));
+;
+var manUnitedWins = 0;
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
+        ++manUnitedWins;
+    }
+    else if (match[2] === 'Man United' && match[5] === matchResult.AwayWin) {
+        ++manUnitedWins;
+    }
+}
+console.log(manUnitedWins);
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+enum matchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+};
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === matchResult.HomeWin ) {
+    ++manUnitedWins;
+  } else if (match[2] === 'Man United' && match[5] === matchResult.AwayWin) {
+    ++manUnitedWins;
+  }
+}
+
+console.log(manUnitedWins);
